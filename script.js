c409007437541c496ff5d574fc2c990afd1f1c1d
cmd-employed <milonfivem11@gmail.com>
2025-12-10 13:59:23 +0100

    Update password in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Pink Anime Snake — script.js
 // Change the password here:
-const PASSWORD = "changeMe123"; // <-- set your password here
+const PASSWORD = "HWDP"; // <-- set your password here
 
 // ---------------- Password overlay handling ----------------
 const pwOverlay = document.getElementById('pwOverlay');
